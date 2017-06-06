0d6cb9dc3aa6c2e162d6ab106cefb0104c744a13
Mário Ribeiro <mail@marioribeiro.com>
2017-06-06 12:17:48 +0100

    Add code for landing planes.
    - Planes are stored in a hangar array

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,11 +1,14 @@
-'use strict'
+'use strict';
 
 describe('Plane', function(){
   var plane;
+  var airport;
   beforeEach(function(){
     plane = new Plane();
+    airport = jasmine.createSpyObj('airport', ['clearForLanding']);
   });
   it('can land at an airport', function(){
-    expect(plane.land).not.toBeUndefined();
+    plane.land(airport);
+    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
 });
--- src/Plane.js
+++ src/Plane.js
@@ -1,4 +1,7 @@
 'use strict';
 
 function Plane() {}
-Plane.prototype.land = function(){};
+
+Plane.prototype.land = function(airport){
+  airport.clearForLanding(this);
+};
