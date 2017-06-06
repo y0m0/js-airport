f2d0d57f9237f8ddad5d52894bd1866980bd586f
y0m0 <lixi.daniele@gmail.com>
2017-06-06 11:52:41 +0100

    Creates Plane class - has a land function - unit tests pass

---
+++ spec/PlaneSpec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+describe('Plane', function(){
+  var plane;
+  beforeEach(function(){
+    plane = new Plane();
+  });
+  it('can land at an airport', function(){
+    expect(plane.land).not.toBeUndefined();
+  });
+});
---
+++ src/Plane.js
@@ -0,0 +1,4 @@
+'use strict';
+
+function Plane() {}
+Plane.prototype.land = function(){};
