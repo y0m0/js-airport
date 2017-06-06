35d6dda952eb4c2adc71ca76b12db90670945713
Mário Ribeiro <mail@marioribeiro.com>
2017-06-06 16:19:33 +0100

    Add weather class with randomizer

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,42 +3,42 @@
 describe('Airport', function(){
   var airport;
   var plane;
+  var weather;
+
   beforeEach(function(){
-    airport = new Airport();
-    plane = jasmine.createSpy('plane', ['land', 'takeoff']);
+    plane = jasmine.createSpy('plane');
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+    airport = new Airport(weather);
   });
 
   it('has no planes by default', function(){
     expect(airport.planes()).toEqual([]);
   });
 
-  it('can clear planes for landing', function(){
-    airport.clearForLanding(plane);
-    expect(airport.planes()).toEqual([plane]);
+  describe('under normal conditions',function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(false);
+    });
+    it('can clear planes for landing', function(){
+      airport.clearForLanding(plane);
+      expect(airport.planes()).toEqual([plane]);
+    });
+    it('can clear planes for takeoff', function(){
+      airport.clearForLanding(plane);
+      airport.clearForTakeoff(plane);
+      expect(airport.planes()).toEqual([]);
+    });
   });
 
-  it('can clear planes for takeoff', function(){
-    airport.clearForTakeoff(plane);
-    expect(airport.planes()).not.toContain(plane);
-  });
-
-  it('check if the weather is stormy', function(){
-    expect(airport.isStormy()).toBeFalsy();
-  });
-
-  describe('under stormy conditions', function(){
-
+  describe('under stormy conditions',function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(true);
+    });
+    it('does not clear planes for landing', function(){
+      expect(function(){ airport.clearForLanding(plane); }).toThrowError('cannot land during storm');
+    });
     it('does not clear planes for takeoff', function(){
-      spyOn(airport, 'isStormy').and.returnValue(true);
       expect(function(){ airport.clearForTakeoff(plane); }).toThrowError('cannot takeoff during storm');
     });
-
-    it('does not clear planes for landing', function(){
-      spyOn(airport, 'isStormy').and.returnValue(true);
-      expect(function() { airport.clearForLanding(plane); }).toThrowError('cannot land during storm');
-    });
-
-  }); 
-
-
+  });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,24 +1,24 @@
 'use strict';
 
-function Airport(){
+function Airport(weather){
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
   this._hangar = [];
 }
-Airport.prototype.planes = function() { return this._hangar; };
 
-Airport.prototype.clearForLanding = function(plane){
-  if (this.isStormy()) {
+Airport.prototype.planes = function(){
+  return this._hangar;
+};
+
+Airport.prototype.clearForLanding = function(plane) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot land during storm');
   }
   this._hangar.push(plane);
 };
 
 Airport.prototype.clearForTakeoff = function(plane) {
-  if (this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot takeoff during storm');
   }
   this._hangar.pop(plane);
-};
-
-Airport.prototype.isStormy = function(){
-  return false;
 };
